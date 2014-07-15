99cc7c13c692a251f38d6457670f1c23f98a85b9
Jim Mello <jmello@g.harvard.edu>
2014-07-14 22:35:10 -0400

    Added default route to point to index, added index and main templates

---
+++ app/views/index.blade.php
@@ -0,0 +1,10 @@
+@extends('main')
+
+@section('title')
+	Developer's Best Friend
+@stop
+
+@section('content')
+	<h1>Developer's Best Friend</h1>
+
+@stop
---
+++ app/views/main.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+
+	<title>@yield('title','Main document')</title>
+
+	@yield('head')
+
+</head>
+
+<body>
+
+	@yield('content')
+
+	@yield('body')
+
+</body>
+
+</html>
