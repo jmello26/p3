236efab90b61a40b2238f795dd63fd02b7c8e6f4
Jim Mello <jmello@g.harvard.edu>
2014-07-16 22:49:57 -0400

    basic routing works for all pages

--- app/routes.php
+++ app/routes.php
@@ -11,17 +11,67 @@
 |
 */
 
+/*
+  Default route for main site -- show index
+*/
 Route::get('/', function()
 {
 	return View::make('index');
 });
 
+/*
+   Handles form submittal
+*/
+Route::post('/', function()
+{
+	$selection = Input::get('dev_tool');
+	if ($selection == 'lorem') {
+		return Redirect::to('/lorem')->withInput(Input::get());
+	}
+	else if ($selection == 'users') {
+		return Redirect::to('/users')->withInput(Input::get());
+	}
+	else {
+		return View::make('index');
+	}
+});
+
+/*
+   Handles direct url to lorem page
+*/
 Route::get('/lorem', function()
 {
 	return View::make('lorem');
 });
 
+/*
+   Handles input posted to lorem page
+*/
+Route::post('/lorem', function() 
+{
+	$num_paras = Input::get('num_paras');
+	$generator = new Badcow\LoremIpsum\Generator();
+	$paragraphs = $generator->getParagraphs($num_paras);
+	$result = implode('<p>', $paragraphs);
+	return View::make('lorem')->with('result', $result);
+});
+
+
+/*
+   Handles direct url to users page
+*/
 Route::get('/users', function()
 {
 	return View::make('users');
+});
+
+
+/*
+   Handles input posted to the users page
+*/
+Route::post('/users', function()
+{
+	$num_users = Input::get('num_users');
+	$result = $num_users;
+	return View::make('users')->with('result', $result);
 });
