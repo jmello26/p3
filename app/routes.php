1060491eb055f063d7db224aad91f1aa65c64547
Jim Mello <jmello@g.harvard.edu>
2014-07-17 12:34:50 -0400

    updated comments

--- app/routes.php
+++ app/routes.php
@@ -19,48 +19,40 @@ Route::get('/', function()
 	return View::make('index');
 });
 
-/*
-   Handles form submittal
-
-Route::post('/', function()
-{
-	$selection = Input::get('dev_tool');
-	if ($selection == 'lorem') {
-		return Redirect::to('/lorem')->withInput(Input::get());
-	}
-	else if ($selection == 'users') {
-		return Redirect::to('/users')->withInput(Input::get());
-	}
-	else {
-		return View::make('index');
-	}
-});
-
-*/
 
 /*
-   Handles direct url to lorem page
+   Direct url requests to the 'lorem' page
 */
 Route::get('/lorem', function()
 {
 	return View::make('lorem');
 });
 
+
 /*
-   Handles input posted to lorem page
+   Input posted to the 'lorem' page
 */
 Route::post('/lorem', function() 
 {
+	// get value from num_paras element
 	$num_paras = Input::get('num_paras');
+	
+	// instantiate lorem ipsum generator
 	$generator = new Badcow\LoremIpsum\Generator();
+	
+	// request paragrpahs of text based on input
 	$paragraphs = $generator->getParagraphs($num_paras);
+	
+	// turn the array into a String
 	$result = implode('<p>', $paragraphs);
+	
+	// pass the result string to this view again
 	return View::make('lorem')->with('result', $result);
 });
 
 
 /*
-   Handles direct url to users page
+   Direct url requests to the 'users' page
 */
 Route::get('/users', function()
 {
@@ -69,16 +61,28 @@ Route::get('/users', function()
 
 
 /*
-   Handles input posted to the users page
+   Input posted to the 'users' page
 */
 Route::post('/users', function()
 {
+
+	// get the value posted from the num_users element
 	$num_users = Input::get('num_users');
+	
+	// instantiate a Faker object
 	$faker = Faker\Factory::create();
+	
+	// array to hold the user information
 	$users = array();
+	
+	// get the requested number of users and store them in the $users array
 	for ($i=0; $i < $num_users; $i++) {
 		$users[$i] = $faker->name;
 	}
+	
+	// convert the array into a String
 	$result = implode('<p>', $users);
+	
+	// re-display this page, passing the string of users back to it
 	return View::make('users')->with('result', $result);
 });
