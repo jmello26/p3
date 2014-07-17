f502203830d7a697ecc74e3bbd9193c2ae1fd916
Jim Mello <jmello@g.harvard.edu>
2014-07-16 22:57:31 -0400

    basics of page working, not getting user info yet

--- app/views/users.blade.php
+++ app/views/users.blade.php
@@ -7,4 +7,15 @@
 @section('content')
 	<h1>Random User Generator</h1>
 
+	{{ Form::open (array( 'url' => '/users', 'method' => 'POST')) }}
+		{{ Form::label('num_users', 'Number of users:') }}
+		<br/>
+		{{ Form::text('num_users'); }} 
+		{{ Form::submit('Submit!'); }}
+	
+	{{ Form::close() }}
+	
+	@if (isset($result))
+		{{ $result }}
+	@endif
 @stop
