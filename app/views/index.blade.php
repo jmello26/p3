ac9dc382b44e30d163dd547eef07e0e24d5d8eae
Jim Mello <jmello@g.harvard.edu>
2014-07-16 22:56:12 -0400

    Added form with radio button to choose a tool

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -7,4 +7,13 @@
 @section('content')
 	<h1>Developer's Best Friend</h1>
 
+	{{ Form::open (array( 'url' => '/', 'method' => 'POST')) }}
+		{{ Form::label('dev_tool', 'Select a tool:') }}
+		<br/>
+		{{ Form::radio('dev_tool', 'lorem'); }} Lorem Ipsum text generator
+		{{ Form::radio('dev_tool', 'users'); }} Random User generator
+		<br/>
+		{{ Form::submit('Submit!'); }}
+	
+	{{ Form::close() }}
 @stop
