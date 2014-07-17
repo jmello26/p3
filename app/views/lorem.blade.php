fed2357bdc9f28520818c79513950aaf56e0d9bd
Jim Mello <jmello@g.harvard.edu>
2014-07-16 22:57:01 -0400

    basics of page working, returning text

--- app/views/lorem.blade.php
+++ app/views/lorem.blade.php
@@ -1,10 +1,23 @@
+
 @extends('main')
 
 @section('title')
-	Lorem Ipsem Generator
+	Lorem Ipsum Generator
 @stop
 
 @section('content')
-	<h1>Lorem Ipsem Generator</h1>
+	<h1>Lorem Ipsum Generator</h1>
 
+	{{ Form::open (array( 'url' => '/lorem', 'method' => 'POST')) }}
+		{{ Form::label('num_paras', 'Number of paragraphs:') }}
+		<br/>
+		{{ Form::text('num_paras'); }} 
+		{{ Form::submit('Get text!'); }}
+	
+	{{ Form::close() }}
+	
+	@if (isset($result))
+		{{ $result }}
+	@endif
+	
 @stop
